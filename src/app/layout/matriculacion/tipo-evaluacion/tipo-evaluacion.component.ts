10464a7c31e2f681bc1d008d46c4b037f35058c8
elias9314 <48599005+elias9314@users.noreply.github.com>
2020-03-20 15:53:04 -0500

    get en el front tipo de evaluacion funciona

--- src/app/layout/matriculacion/tipo-evaluacion/tipo-evaluacion.component.ts
+++ src/app/layout/matriculacion/tipo-evaluacion/tipo-evaluacion.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {TipoEvaluacion} from '../modelos/tipo-evaluacion.model';
 import { ServiceService } from '../service.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+
+
 @Component({
   selector: 'app-tipo-evaluacion',
   templateUrl: './tipo-evaluacion.component.html',
   styleUrls: ['./tipo-evaluacion.component.scss']
 })
 export class TipoEvaluacionComponent implements OnInit {
-  tipoevaluaciones: any = [];
-  data: any;
+  tipoevaluaciones: any;
+
   tipoevaluacion: TipoEvaluacion;
 
-  constructor(private service: ServiceService) { }
+  constructor(private spinner: NgxSpinnerService, private service: ServiceService) { }
 
   ngOnInit() {
     this.getTipoEvaluacion();
   }
-  getTipoEvaluacion() {
 
+  getTipoEvaluacion() {
+    this.spinner.show();
     this.service.get('tipo_evaluaciones').subscribe(
       response => {
         this.tipoevaluaciones = response;
-      },
-      error => {
+        console.log(response);
+        this.spinner.hide();
+                 },
+    error => {
+        this.spinner.hide();
         console.log('error');
-      }
-    );
+              });
+        }
   }
-
-}
